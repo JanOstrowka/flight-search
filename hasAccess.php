ac8f64c45bedf1ca24495403f14d92e420115bb1
Jan Ostrówka <gitlab@ostrowka.com>
2020-02-19 21:21:31 +0100

    login bug fix

--- hasAccess.php
+++ hasAccess.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if (!isset($_SESSION['sLogin'])){
-        header('Location: login.php');
+        header('Location: login.css');
         exit();
     }
 ?>
--- login.php
+++ login.php
@@ -22,15 +22,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/login">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
     <title>LOGIN</title>
 </head>
 <body>
-<h1>HOME</h1>
-<form action="login.php" method="POST">
-    <input name="txtLogin" type="text" placeholder="login">
-    <input name="txtPassword" type="text" placeholder="password">
-    <button>LOGIN</button>
-</form>
+    <form action="login.php" method="POST">
+        <h1>HOME</h1>
+        <input name="txtLogin" type="text" placeholder="login">
+        <input name="txtPassword" type="text" placeholder="password">
+        <button>LOGIN</button>
+    </form>
 </body>
 </html>
